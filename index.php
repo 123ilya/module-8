68986aab7047a049f188fffae82e4a4e2b6cbc8d
Илья Соболев <123ilya@gmail.com>
2022-09-13 13:28:21 +1000

    second commit

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
 <?php
+
+// Получаем содержимое файла
+//$g = file_get_contents('.\test.txt');
+//Изменяем содержимое файла
+//file_put_contents('.\test.txt', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+//echo $g;
+//Проверяем сущуществование файла или каталога
+//echo file_exists('.\test.txt');
+//-----------------------------------------------------------------------------------------------------------
+// Создаю класс TelegraphText и добавляю в него соответствующие поля
+class TelegraphText
+{
+    //Добавляю поля
+    public $text, $title, $author, $published, $slug;
+
+//Добавляю конструктор. При создании объекта ,конструктор инициализирует, указанные в нём переменные
+    public function __construct($author, $slug)
+    {
+        $this->author = $author;
+        $this->slug = $slug;
+        $this->published = date('Y-m-d H:i:s');
+    }
+
+    public function storeText($title, $text)
+    {
+        $arr = [
+            'title' => $title,
+            'text' => $text,
+            'author' => $this->author,
+            'published' => $this->published
+        ];
+        foreach ($arr as $item) {
+            echo $item . PHP_EOL;
+        }
+    }
+}
+$aaa = new TelegraphText('ilya','ttt');
+$aaa->storeText('gope','morz b solntce');
